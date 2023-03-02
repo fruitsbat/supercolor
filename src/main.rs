51648d872c9f4b7477f005dc76e261b0e267080e
zoe <ZoeOosting@outlook.com>
2023-03-03 00:54:34 +0100

    add supercolor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use clap::{Parser, ValueEnum};
-use colored::*;
-use pad::PadStr;
-use rand::{distributions::WeightedIndex, prelude::*, Rng};
+use clap::Parser;
+use rand::{distributions::WeightedIndex, prelude::*};
 use std::{fs, path::PathBuf};
 use strum::IntoEnumIterator;
 
 mod builtins;
+mod print;
 
 // test the example with `cargo run --example most_simple`
 fn main() {
@@ -57,26 +56,12 @@ fn main() {
                 .expect("failed to read file")
         }
     };
-    print(data, &args);
+    print::print(data, &args);
 }
 
 enum DataType {
     Builtin,
     File,
-}
-
-/// give number of rainbow color n this returns string in that color
-fn rainbowify<S: Into<String>>(input: S, number: u64) -> ColoredString {
-    match number.rem_euclid(6) {
-        0 => input.into().red(),
-        1 => input.into().green(),
-        2 => input.into().yellow(),
-        3 => input.into().blue(),
-        4 => input.into().magenta(),
-        5 => input.into().cyan(),
-        // waht the fuck
-        _ => input.into().red(),
-    }
 }
 
 fn read_path(args: &Args) -> Vec<PathBuf> {
@@ -106,52 +91,9 @@ fn read_path(args: &Args) -> Vec<PathBuf> {
     bufs
 }
 
-fn print<S: Into<String>>(input: S, args: &Args) {
-    let rows = args.rows;
-    let columns = args.columns;
-    let input: String = input.into();
-    let mut longest: usize = 0;
-    let offset = args
-        .color_offset
-        .unwrap_or(rand::thread_rng().gen_range(0..5));
-
-    for line in input.lines() {
-        if line.chars().count() > longest {
-            longest = line.chars().count();
-        }
-    }
-
-    let mut linecount = 0;
-    for row in 0..rows {
-        for line in input.lines() {
-            linecount += 1;
-            for column in 0..columns {
-                let color = (offset
-                    + match args.color_mode {
-                        ColorMode::Solid => column + row,
-                        ColorMode::Uniform => 0,
-                        ColorMode::Rainbow => linecount,
-                    }) as u64;
-                if column == columns - 1 {
-                    println!("{}", rainbowify(line, color));
-                } else {
-                    print!("{}", rainbowify(line.pad_to_width(longest), color))
-                }
-            }
-        }
-    }
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
-enum ColorMode {
-    Solid,
-    Rainbow,
-    Uniform,
-}
-
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
-struct Args {
+pub struct Args {
     /// how many times it will be printed horizontally
     #[arg(short, long, default_value_t = 1)]
     rows: u8,
@@ -161,17 +103,19 @@ struct Args {
     columns: u8,
 
     /// which mode to use
-    #[arg(long, value_enum, default_value = "solid")]
-    color_mode: ColorMode,
+    #[arg(long, value_enum, num_args = 1.., value_delimiter = ' ')]
+    color_modes: Vec<print::ColorMode>,
 
     /**
     color to start with, uses random if not set
-    0 = red
-    1 = green
-    2 = yellow
-    3 = blue
-    4 = magenta
-    5 = cyan
+    0 = black
+    1 = red
+    2 = green
+    3 = yellow
+    4 = blue
+    5 = magenta
+    6 = cyan
+    7 = white
     */
     #[arg(long)]
     color_offset: Option<u8>,
